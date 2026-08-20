cbd643ec2cf15fc35ae493e6037a6c3f7912a8ad
HyosangKang <hkang4224@gmail.com>
2026-08-20 05:35:41 +0000

    Deploying to gh-pages from @ HyosangKang/HyosangKang.github.io@e0a3ab65df09a9e12a8e11cc279df8a7b5d4bdb9 🚀

--- scripts/build_games.sh
+++ scripts/build_games.sh
@@ -9,7 +9,7 @@ if [ ! -f "$wasm_exec" ]; then
   exit 1
 fi
 
-for game_name in fill predator rocket rotate throw virus stone-skipping math-arcade
+for game_name in fill predator rocket rotate throw virus stone-skipping math-arcade lanchester aes-challenge
 do
   source_dir="$repo_root/game-source/$game_name"
   output_dir="$repo_root/game/$game_name"
